c5b6f08e54a1f0a3c1f44d62794b06f151f772d0
OlaAkindele <akindele.ok@gmail.com>
2024-10-05 09:09:55 +0100

    train function

--- src/modules/build_model_test.py
+++ src/modules/build_model_test.py
@@ -1,7 +1,10 @@
 from sklearn.ensemble import ExtraTreesRegressor
 from util import print_metrics
 
+# define function to train the dataset
 def train_model(train_x, train_y):
+  # we will use ExtraTreesRegressor for this task
+    # Define the necessary parameters 
     model = ExtraTreesRegressor(n_estimators=100, 
                                 max_depth=12,
                               bootstrap=True, 
